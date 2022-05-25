97841d77c5d8e64122c6a044d55554ac48702fd0
nizmo180sx <93930354+nizmo180sx@users.noreply.github.com>
2022-05-25 17:55:10 +0700

    Update resume.module.ts

--- server/src/resume/resume.module.ts
+++ server/src/resume/resume.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { memoryStorage } from 'multer';
+import { mkdir } from 'fs/promises';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
 
 import { AuthModule } from '@/auth/auth.module';
+import { User } from '@/users/entities/user.entity';
 import { UsersModule } from '@/users/users.module';
 
 import { Resume } from './entities/resume.entity';
@@ -15,6 +18,24 @@ import { ResumeService } from './resume.service';
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([Resume]),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: async (req, _, cb) => {
+          const userId = (req.user as User).id;
+          const resumeId = +req.params.id;
+          const destination = join(__dirname, '..', `assets/uploads/${userId}/${resumeId}`);
+
+          await mkdir(destination, { recursive: true });
+
+          cb(null, destination);
+        },
+        filename: (_, file, cb) => {
+          const filename = new Date().getTime() + extname(file.originalname);
+
+          cb(null, filename);
+        },
+      }),
+    }),
     MulterModule.register({ storage: memoryStorage() }),
     AuthModule,
     UsersModule,
